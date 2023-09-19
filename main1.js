23097c84885f2474e430ef899e4bebfe68f8e808
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-09-19 07:06:47 -0500

    docs: :memo: Cambio js

---
+++ main1.js
@@ -0,0 +1,23 @@
+let continente = {
+    americaDelSur:{ 
+        poblacion:445898814
+        }
+    }
+let pais = {
+    colombia:{ 
+        poblacion: 51609000
+        }
+    }
+let departamento = {
+    santander:{ 
+        poblacion:2185000
+    }
+    }
+let ciudad = {
+    bucaramanga:{
+        poblacion: 581130
+    }
+}
+let barrio = {
+    granjasDeProvenza: 5000
+}
