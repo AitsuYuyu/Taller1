c4679d7183a3012998cbb6d0993df84dc61d827a
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-09-19 07:25:25 -0500

    feat: :construction: avance en el primer problema

--- main1.js
+++ main1.js
@@ -1,9 +1,10 @@
+/*
 let continente = {
     americaDelSur:{ 
         poblacion:445898814
         }
     }
-let pais = {
+let pais1 = {
     colombia:{ 
         poblacion: 51609000
         }
@@ -20,4 +21,34 @@ let ciudad = {
 }
 let barrio = {
     granjasDeProvenza: 5000
+}*/
+
+const taller = {
+    continente: {
+        americaDelSur: {
+            poblacionContinente: 445898814
+        }
+    },
+
+    pais:{
+        colombia: {
+            poblacionPais: 51609000
+        }
+    },
+    departamento:{
+        santander:{
+            poblacionDepartamento:2185000 
+        }
+    },
+    ciudad:{
+        bucaramanga:{
+            poblacionCiudad:581130
+        }
+    },
+    barrio:{
+        granjasDeProvenza:{
+            poblacionBarrio:5000
+        }
+    }
 }
+console.log(taller)
